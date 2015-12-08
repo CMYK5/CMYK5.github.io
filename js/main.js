1f9202c9e262e0b2a51ab8e101da91ff741bcf42
Sean <seanmurphy@Seans-MacBook-Pro.local>
2015-12-07 20:56:44 -0500

    Rest o the stuff

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//Credit to Lucas Bebber -> 
+
+$(document).ready(function() {
+  var $img = $("img");
+  var data = $img.attr("src");
+  var maxErrors = 100;
+  var margin = 2200;
+
+  function update() {
+    var corrupted = data;
+    if (Math.random() > 0.7) {
+      var errors = Math.round(Math.random() * maxErrors)
+      for (var i = 0; i < errors; i++) {
+        var p = margin + Math.round(Math.random() * (corrupted.length - margin - 1));
+        corrupted = corrupted.substr(0, p) + corrupted.charAt(p + 1) + corrupted.charAt(p) + corrupted.substr(p + 2);
+      }
+    }
+    $img.attr("src", corrupted);
+
+  }
+  update();
+  setInterval(update, 120);
+  
+});
+
