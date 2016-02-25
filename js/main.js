42a07837a94913f1aa8173a5da8c8abf381591aa
Sean <seanmurphy@Seans-MacBook-Pro.local>
2016-02-24 21:11:32 -0500

    Added name
    w/js to toggle on mouseover

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//Credit to Lucas Bebber -> 
+//Credit to Lucas Bebber vvvvv 
 
 $(document).ready(function() {
   var $img = $("img");
@@ -21,5 +21,10 @@ $(document).ready(function() {
   update();
   setInterval(update, 120);
   
+  $("#name").hover(function() {
+    $(this).attr("src","img/Sean Murphy obf.png");
+      }, function() {
+    $(this).attr("src","img/Sean Murphy.png");
+  });
 });
 
